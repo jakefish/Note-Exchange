ebf0672e172e2053a7bcae865b224a473c9dcf98
fishb1jw <fishb1jw@cmich.edu>
2016-04-21 10:10:29 -0400

    remove fb

--- note_exchange/settings.py
+++ note_exchange/settings.py
@@ -1,15 +1,5 @@
-"""
-Django settings for note_exchange project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 TEMPALTE_DIRS = (
@@ -23,13 +13,10 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '0q6n7479#!#bn20^3+b)_8r7&n#yn=2kh&5rnx1p@_fb$ooc_r'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 TEMPLATE_DEBUG = True
@@ -39,7 +26,7 @@ ALLOWED_HOSTS = []
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
-# Application definition
+
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -70,8 +57,7 @@ ROOT_URLCONF = 'note_exchange.urls'
 WSGI_APPLICATION = 'note_exchange.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
@@ -80,8 +66,7 @@ DATABASES = {
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -94,15 +79,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(PROJECT_ROOT, 'static')
 
 
-FACEBOOK_APP_ID='1161479200538439'
-FACEBOOK_API_SECRET='79942e89fa911ac5277c6185e9c07a5a'
+
 FACEBOOK_EXTENDED_PERMISSIONS = ['email']
 FACEBOOK_PROFILE_EXTRA_PARAMS = {'locale': 'ru_RU'}
 
