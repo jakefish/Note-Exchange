0bbc19b0e7c27f0d5aae217b4c5ec025a69beb72
Jake Fish <jacobwfish@gmail.com>
2016-05-02 15:27:35 -0400

    Refactor note exchange

--- note_exchange/urls.py
+++ note_exchange/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
-import haystack
 from django.contrib import admin
+
+import haystack
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-  #  (r'^login/$', 'login.views.login_user'),
   url(r'^admin/', include(admin.site.urls)),
   url(r'', include('social_auth.urls')),
   url(r'^search/', include('haystack.urls'), name="haystack_search"),
